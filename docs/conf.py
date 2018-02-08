3953ed7cafbfa9d2d86ff540ce3aaf1996adc78f
Erwan Pannier <erwan.pannier@gmail.com>
2018-02-08 13:19:49 +0100

    tried to remove old napoleoncontrib

--- docs/conf.py
+++ docs/conf.py
@@ -37,8 +37,8 @@ extensions = [
     #'sphinx.ext.viewcode',
     'sphinx.ext.autosummary', 
     #'numpydoc', 
-    'sphinxcontrib.napoleon',
-    #'sphinx.ext.napoleon',
+    #'sphinxcontrib.napoleon',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
