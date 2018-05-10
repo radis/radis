a38595377eb12ec5457a19698f2b2475711d7ea5
Erwan <erwan.pannier@centralesupelec.fr>
2018-05-11 00:13:24 +0200

    update cache file functions to deal with non str formats

--- radis/misc/cache_files.py
+++ radis/misc/cache_files.py
@@ -7,7 +7,7 @@ import h5py
 import radis
 from warnings import warn
 from os.path import exists
-from radis.misc.basics import compare_dict
+from radis.misc.basics import compare_dict, is_float
 class DeprecatedFileError(DeprecationWarning):
     pass
 
@@ -44,9 +44,14 @@ def check_not_deprecated(file, metadata, current_version=None, last_compatible_v
     
     '''
     
+    # Get attributes (metadata+version)
     hf = h5py.File(file, 'r')
-    attrs = dict(hf.attrs)
-    hf.close()
+    try:
+        attrs = dict(hf.attrs)
+    except OSError:
+        attrs = {}
+    finally:
+        hf.close()
     
     # Raise an error if version is not found
     try:
@@ -80,14 +85,26 @@ def check_not_deprecated(file, metadata, current_version=None, last_compatible_v
         raise ValueError('File generated with a future version?')
         
     # Compare metadata
+    metadata = _h5_compatible(metadata)
     out, compare_string = compare_dict(metadata, attrs, verbose=False, return_string=True)
     if out != 1:
         raise DeprecatedFileError('Metadata in file {0} dont match '.format(file)+\
                          'expected values. See comparison below:'+\
-                         '\nExpected\tFile\n{0}'.format(compare_string))
+                         '\n\tExpected\tFile\n{0}'.format(compare_string))
 
     return out
-    
+
+def _h5_compatible(a_dict):
+    ''' Make dictionary ``a_dict`` h5 compatible '''
+    out = {}
+    for k, v in a_dict.items():
+        if v is None:  
+            continue   # dont store None
+        elif is_float(v):
+            out[k] = v
+        else:
+            out[k] = str(v)   # convert to str
+    return out
         
 def save_to_hdf(df, fname, metadata, version=None, key='df', overwrite=True):
     ''' Save energy levels to HDF5 file. Add metadata and version 
@@ -103,7 +120,8 @@ def save_to_hdf(df, fname, metadata, version=None, key='df', overwrite=True):
         
     metadata: dict
          dictionary of values that were used to generate the DataFrame. Metadata
-         will be asked again on file load to ensure it hasnt changed. 
+         will be asked again on file load to ensure it hasnt changed. ``None`` 
+         values will not be stored.
     
     version: str, or ``None``
         file version. If ``None``, the current :data:`radis.__version__` is used. 
@@ -117,24 +135,39 @@ def save_to_hdf(df, fname, metadata, version=None, key='df', overwrite=True):
     overwrite: boolean
         if ``True``, overwrites file. Else, raise an error if it exists.
     
+    Notes
+    -----
+    
+    ``None`` values are not stored
+    
     '''
     
     assert fname.endswith('.h5')
     assert 'version' not in metadata
+    
+    # Update metadata format
+    metadata = _h5_compatible(metadata)
 
     # Overwrite file    
     if exists(fname) and not overwrite:
         raise ValueError('File exist: {0}'.format(fname))
     hf = h5py.File(fname, 'w')
-
-    # Start by adding version
-    if version is None: version = radis.__version__
-    hf.attrs['version'] = version
     
-    # Add metadata
-    hf.attrs.update(metadata)
+    try:
     
-    hf.close()
+        # Start by adding version
+        if version is None: version = radis.__version__
+        hf.attrs['version'] = version
+        
+        # Add metadata
+        for k, v in metadata.items():
+            hf.attrs[k] = v
+            
+    except:
+        raise
+                
+    finally:
+        hf.close()
     
     # now export dataframe
     df.to_hdf(fname, key, format='fixed', mode='a', complevel=1, complib='blosc')
@@ -148,6 +181,7 @@ def filter_metadata(arguments, discard_variables=['self', 'verbose']):
         
     - remove variables in ``discard_variables``
     - remove variables that start with ``'_'``
+    - remove varibles whose value is ``None``
     
     Parameters
     ----------
@@ -186,6 +220,7 @@ def filter_metadata(arguments, discard_variables=['self', 'verbose']):
     
     metadata = {k:v for (k,v) in arguments.items() if not k.startswith('_')}
     metadata = {k:v for (k,v) in metadata.items() if k not in discard_variables}
+    metadata = {k:v for (k,v) in metadata.items() if v is not None}
     
     return metadata
     
