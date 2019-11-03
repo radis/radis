d2d2fd113223321bd8fe6aec6a4c82eb11669b94
Erwan <erwan.pannier@centralesupelec.fr>
2019-11-04 00:30:50 +0100

    (misc) nicely print m^-1

--- radis/spectrum/utils.py
+++ radis/spectrum/utils.py
@@ -112,6 +112,7 @@ def make_up(label):
     # Improve units
     label = label.replace(r'cm_1', r'cm-1')
     label = label.replace(r'cm-1', r'cm$^\mathregular{-1}$')
+    label = label.replace(r'm^-1', r'm$^\mathregular{-1}$')
     label = label.replace(r'm2', r'm$^\mathregular{2}$')
     label = label.replace(r'm3', r'm$^\mathregular{3}$')
     label = label.replace(r'I/I0', r'I/I$_\mathregular{0}$')    # transmittance unit
