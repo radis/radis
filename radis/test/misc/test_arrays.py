4e317fc3c56d92641c8f12b3253c487711140362
Pankaj Mishra <pankajmishra1511@gmail.com>
2020-04-11 19:02:26 +0530

    added tests for misc array functions

--- radis/test/misc/test_arrays.py
+++ radis/test/misc/test_arrays.py
@@ -7,7 +7,7 @@ Created on Wed Aug 29 10:35:24 2018
 
 from __future__ import print_function, absolute_import, division, unicode_literals
 import numpy as np
-from radis.misc.arrays import is_sorted, is_sorted_backward
+from radis.misc.arrays import is_sorted, is_sorted_backward, find_first, bining, shift_array, find_nearest, calc_diff
 
 
 def test_is_sorted(*args, **kwargs):
@@ -21,12 +21,67 @@ def test_is_sorted(*args, **kwargs):
     assert not is_sorted(a[::-1])
 
 
+def test_find_first(*args, **kwargs):
+
+    a = np.arrange(10)
+    assert find_first(a, -1) == 0
+    assert find_first(a, 0) == 1
+    assert find_first(a, 5) == 6
+    assert find_first(a, 8) == 9
+    assert find_first(a, 9) == 0
+    assert find_first(a, 20) == 0
+
+    assert not find_first(a, -1) == -1
+    assert not find_first(a, 0) == 0
+    assert not find_first(a, 5) == 5
+    assert not find_first(a, 10) == 10
+
+
+def test_bining(*args, **kwargs):
+
+    a = np.arange(20).reshape(4, 5)
+    assert(bining(a) == np.array([2, 7, 12, 17])).all()
+    assert (bining(a, ymin=1) == np.array([2.5, 7.5, 12.5, 17.5])).all()
+    assert (bining(a, ymax=3) == np.equal([1, 6, 11, 16])).all()
+    assert (bining(a, ymin=1, ymax=3) == np.equal([1.5, 6.5, 11.5, 16.5])).all()
+
+
+#TODO:
+#def test_find_nearest(*args, **kwargs):
+
+#def test_shift_array(*args, **kwargs):
+
+def test_calc_diff(*args, **kwargs):
+    t1 = np.arange(5)
+    t2 = np.arange(5)
+
+    v1 = np.array([10, 12, 14, 16, 18])
+    v2 = np.array([10, 12, 14, 16, 18])
+
+    t_res1, v_res1 = calc_diff(t1, v1, t2, v2)
+    t_res2, v_res2 = calc_diff(t1, v1, t1[::-1], v2)
+    t_res3, v_res3 = calc_diff(t2[::-1], v1, t2, v2)
+
+    assert (t_res1 == np.array([2, 3, 4])).all()
+    assert (v_res1 == np.array([0, 0, 0])).all()
+
+    assert (t_res2 == np.array([2, 3, 4])).all()
+    assert (v_res2 == np.array([-4, 0, 4])).all()
+
+    assert (t_res3 == np.array([2, 3, 4])).all()
+    assert (v_res3 == np.array([4, 0, -4])).all()
+
+
+
 def _run_testcases(verbose=True, *args, **kwargs):
     """ Test array functions
 
     """
 
     test_is_sorted()
+    test_find_first()
+    test_bining()
+    test_calc_diff()
 
     return True
 
