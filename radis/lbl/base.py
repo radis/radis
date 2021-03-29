1f4524e55e9ca59448585d3bba17e03f05465bae
Gagan Aryan <gaganaryan19@gmail.com>
2021-03-30 04:55:34 +0530

    fix: #214

--- radis/lbl/base.py
+++ radis/lbl/base.py
@@ -3228,7 +3228,7 @@ class BaseFactory(DatabankLoader):
 def get_waverange(
     wmin=None,
     wmax=None,
-    wunit=None,
+    wunit=Default('cm-1'),
     wavenum_min=None,
     wavenum_max=None,
     wavelength_min=None,
