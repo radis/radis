cb7a28d02c87d57f47a2e87cf30306cf769312a3
Erwan Pannier <erwan.pannier@gmail.com>
2021-08-06 10:59:46 +0200

    plot hitemp OH

--- examples/plot_hitemp_OH_database.py
+++ examples/plot_hitemp_OH_database.py
@@ -13,7 +13,7 @@ To compute a spectrum with the HITEMP database, see the
 
 from radis.io.hitemp import fetch_hitemp
 
-df = fetch_hitemp("CO")
+df = fetch_hitemp("OH")
 print(df.columns)
 
 
@@ -21,9 +21,16 @@ print(df.columns)
 Returns:
 ::
     Index(['id', 'iso', 'wav', 'int', 'A', 'airbrd', 'selbrd', 'El', 'Tdpair',
-       'Pshft', 'ierr', 'iref', 'lmix', 'gp', 'gpp', 'Fu', 'branch', 'jl',
-       'syml', 'Fl', 'vu', 'vl'],
-      dtype='object')
+           'Pshft', 'globu', 'globl', 'locu', 'locl', 'ierr', 'iref', 'lmix', 'gp',
+           'gpp'],
+          dtype='object')
 
 Columns are described in :py:attr:`~radis.io.hitran.column_2004`
+
+A specific can be retrieved with the same :py:func:`~radis.io.hitemp.fetch_hitemp`
+function. The already downloaded database will be used:
 """
+
+fetch_hitemp("OH", load_wavenum_min=31500, load_wavenum_max=33000, isotope="1").plot(
+    "wav", "int"
+)
