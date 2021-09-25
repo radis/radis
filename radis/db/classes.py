491854fd6b9c3946486ff6c0e3d091353b9c538f
minou <minesi89@gmail.com>
2021-09-25 13:27:31 -0700

    hard coded isotopologue name and mass of CN

--- radis/db/classes.py
+++ radis/db/classes.py
@@ -281,6 +281,7 @@ def get_molecule(molecule_id):
 KNOWN_EXOMOL_ISOTOPES_NAMES = {
     ("FeH", 1): "56Fe-1H",
     ("SiO", 1): "28Si-16O",  # Placeholder until all molecules parsed from website TODO
+    ("CN", 1): "12C-14N",  # Placeholder until all molecules parsed from website TODO
 }
 """All :py:data:`~radis.db.classes.HITRAN_MOLECULES` are also converted to their ExoMol full-name format
 in :py:func:`~radis.io.exomol.get_exomol_full_isotope_name`
