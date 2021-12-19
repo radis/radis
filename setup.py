9af6f07d402510524cefd8a086b00e0d0652399c
Erwan Pannier <erwan.pannier@gmail.com>
2021-12-19 19:31:30 +0100

    update vaex version

--- setup.py
+++ setup.py
@@ -277,7 +277,7 @@ def run_setup(with_binary):
             "seaborn",  # other matplotlib themes
             "scipy>=1.4.0",
             "tuna",  # to generate visual/interactive performance profiles
-            "vaex>=4.7.0",  # load HDF5 files  (version for custom HDF5 groups). #TODO : install only required sub-packages
+            "vaex>=4.6.0",  # load HDF5 files  (version for custom HDF5 groups + avoids asyncio https://github.com/vaexio/vaex/pull/1546). #TODO : install only required sub-packages
         ],
         extras_require={
             "dev": [
