ffe781c4a4b2f0fdb3c574c5fffdd4ac76d5f4c2
gagan-aryan <gagan@iitk.ac.in>
2022-03-25 23:25:54 +0530

    fix: lint errors

--- radis/lbl/gpu.py
+++ radis/lbl/gpu.py
@@ -609,6 +609,7 @@ def gpu_iterate(p, T, mole_fraction, l=1.0, slit_FWHM=0.0, verbose=0, gpu=False)
     else:
         from numpy import complex64, float32, zeros
         from numpy.fft import irfft, rfft
+
         from radis_cython_extensions import (
             applyGaussianSlit,
             applyLineshapes,
