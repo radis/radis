e0c5c05e9a5c555cc9fb77755ae3d4c8b4cc9b8f
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-08-14 23:30:05 -0400

    spelling: bound
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- radis/spectrum/rescale.py
+++ radis/spectrum/rescale.py
@@ -3,7 +3,7 @@
 new spectral quantities that can be derived from existing ones, or rescale
 path_length or mole_fraction, or add overpopulations.
 
-Most of these are binded as methods to the Spectrum class, but stored here to
+Most of these are bound as methods to the Spectrum class, but stored here to
 unload the spectrum.py file
 
 
