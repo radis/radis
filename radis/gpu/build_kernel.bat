1444eadfd32b9138da984d6634611965d6dc8acf
Dirk van den Bekerom <dcmvdbekerom@gmail.com>
2023-08-15 19:30:25 +0200

    clean up build scripts

--- radis/gpu/build_kernel.bat
+++ radis/gpu/build_kernel.bat
@@ -1,11 +1,13 @@
 cls
-nvcc --ptx kernels.cu -ccbin "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\bin" --define-macro __CUDACC__
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+
+nvcc --ptx kernels.cu
 
 copy kernels.cu kernels.cpp
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 cl /LD kernels.cpp /IMPLIB
 del kernels.lib
 del kernels.exp
 del kernels.obj
 del kernels.cpp
+
 pause
--- radis/gpu/build_test_kernel.bat
+++ radis/gpu/build_test_kernel.bat
@@ -1,11 +1,14 @@
 cls
-nvcc --ptx test_kernel.cu -ccbin "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\bin" --define-macro __CUDACC__
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+
+nvcc --ptx test_kernel.cu
 
 copy test_kernel.cu test_kernel.cpp
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 cl /LD test_kernel.cpp /IMPLIB
 del test_kernel.lib
 del test_kernel.exp
 del test_kernel.obj
 del test_kernel.cpp
+
 pause
