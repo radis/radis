f5b6c2fac639c49c928d60192423b1f9512c05ee
Erwan Pannier <erwan.pannier@gmail.com>
2023-08-16 00:40:41 +0200

    add a numba & cython version of lineshape aggregation (standard optimization=None version without adaptative grid)

--- radis/misc/arrays.py
+++ radis/misc/arrays.py
@@ -523,9 +523,67 @@ def numpy_add_at(LDM, k, l, m, I):
 
 # %%
 # Functions for legacy aggregation of lineshapes over an array
-
-
 def numpy_aggregate_at_indices(
+    sumoflines_calc,
+    I_low_in_left,
+    I_low_in_right,
+    I_high_in_left,
+    I_high_in_right,
+    profile_S_T,
+    frac_left,
+    frac_right,
+):
+    for i, (fr_left, fr_right, profS) in enumerate(
+        zip(frac_left, frac_right, profile_S_T)
+    ):
+        sumoflines_calc[I_low_in_left[i] : I_high_in_left[i] + 1] += fr_left * profS
+        sumoflines_calc[I_low_in_right[i] : I_high_in_right[i] + 1] += fr_right * profS
+    return sumoflines_calc
+
+
+@numba.njit(fastmath=True)  # eq. O3, march-native,fastmath
+def numba_aggregate_at_indices(
+    sumoflines_calc,
+    I_low_in_left,
+    I_low_in_right,
+    I_high_in_left,
+    I_high_in_right,
+    profile_S_T,
+    frac_left,
+    frac_right,
+):
+    N_lines = len(profile_S_T)
+
+    # loop over all lines
+    for i in range(N_lines):
+        # we keep track of 2 index: one `j` for the profile [0-len(profS)]
+        # and one `k` for the corresponding position in sumoflines_calc, which
+        # has jumps because the profile is hollow
+        j = 0
+        # Left side:
+        for k in range(I_low_in_left[i], I_high_in_left[i] + 1):
+            sumoflines_calc[k] += frac_left[i] * profile_S_T[i][j]
+            j += 1
+        # Right side:
+        j = 0
+        for k in range(I_low_in_right[i], I_high_in_right[i] + 1):
+            sumoflines_calc[k] += frac_right[i] * profile_S_T[i][j]
+            j += 1
+
+    return sumoflines_calc
+
+
+if (
+    False
+):  # EP 15/08 : as of manual benchmark, Cython version is 10x slower than numba version. Deactivated
+    import radis_cython_extensions as rcx
+
+    aggregate_at_indices = rcx.aggregate_at_indices_64
+# aggregate_at_indices = numpy_aggregate_at_indices
+aggregate_at_indices = numba_aggregate_at_indices
+
+
+def numpy_aggregate_at_indices_hollow(
     sumoflines_calc,
     I_low_in_left,
     I_low_in_right,
@@ -566,7 +624,7 @@ def numpy_aggregate_at_indices(
 
 
 @numba.njit(fastmath=True)  # eq. O3, march-native,fastmath
-def numba_aggregate_at_indices(
+def numba_aggregate_at_indices_hollow(
     sumoflines_calc,
     I_low_in_left,
     I_low_in_right,
@@ -607,14 +665,14 @@ def numba_aggregate_at_indices(
     return sumoflines_calc
 
 
-if False:
+if (
+    False
+):  # EP 15/08 : as of manual benchmark, Cython version is 10x slower than numba version. Deactivated
     import radis_cython_extensions as rcx
 
-    aggregate_at_indices = (
-        rcx.aggregate_at_indices_64
-    )  # EP 15/08 : as of manual benchmark, Cython version is 10x slower than numba version
+    aggregate_at_indices = rcx.aggregate_at_indices_hollow_64
 # aggregate_at_indices = numpy_aggregate_at_indices
-aggregate_at_indices = numba_aggregate_at_indices
+aggregate_at_indices_hollow = numba_aggregate_at_indices_hollow
 
 
 # %%
