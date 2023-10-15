88371cd8a66dadd8d4ce60c5cca29792d5c1a73e
Dirk van den Bekerom <dcmvdbekerom@gmail.com>
2023-10-15 14:51:18 +0200

    Working test spectrum (part.fun. and coll.dep.broadening)

--- radis/gpu/vulkan/test_dummy_spectrum3.py
+++ radis/gpu/vulkan/test_dummy_spectrum3.py
@@ -31,25 +31,20 @@ v_max = 2450.0
 dv = 0.002
 N_v = 150001
 N_v_FT = next_fast_len_even(2 * N_v)
-print("N_v = {:d}".format(N_v))
+print("N_v    = {:d}".format(N_v))
+print("N_v_FT = {:d}".format(N_v_FT))
 v_arr = np.arange(N_v) * dv + v_min
 x_arr = rfftfreq(N_v_FT, dv)
 N_x_FT = len(x_arr)
 Ntpb = 1024  # threads per block
-w0 = 0.2
-# N_lines = 100
-T0 = 1500.0
-p0 = 1.0  # bar
 
+T0 = 3000.0
+p0 = 1.0  # bar
+x = 0.8
 
 log_2p = np.log(2 * p0)
+hlog_T = 0.5 * np.log(T0)
 log_rT = np.log(296.0 / T0)
-
-Mm_list = [4.3989830e01, 4.4993183e01, 4.5994076e01]
-
-log_c2Mm_arr = c_float_arr_16(
-    0, *[0.5 * np.log(8 * k * np.log(2) / (c**2 * Mm * 1e-3 / N_A)) for Mm in Mm_list]
-)
 
 
 def init_w_axis(dx, log_wi):
@@ -63,14 +58,26 @@ def init_w_axis(dx, log_wi):
 database_arrays = [np.load("data_arr_{:d}.npy".format(i)) for i in range(7)]
 N_lines = database_arrays[0].size
 
-dxL = 0.2
+dxG = 0.13753508031368256
+dxL = 0.20180289447307587
+
+# Calc G params:
+Mm_list = [4.3989830e01, 4.4993183e01, 4.5994076e01]
+iso = database_arrays[0]
+v0 = database_arrays[1]
+log_c2Mm_arr = c_float_arr_16(
+    0, *[0.5 * np.log(8 * k * np.log(2) / (c**2 * Mm * 1e-3 / N_A)) for Mm in Mm_list]
+)
+log_wG_data0 = np.log(v0) + np.array(log_c2Mm_arr)[iso]
+log_wG_data = log_wG_data0 + hlog_T
+log_wG_min, log_wG_max, N_G = init_w_axis(dxG, log_wG_data)
+
+
+# Calc L params:
 na = database_arrays[-2]
 gamma = database_arrays[-1][0]
-
-print(na.shape)
-print(gamma.shape)
-log_wL_data = np.log(gamma) + log_2p + na * log_rT
-
+log_wL_data0 = np.log(gamma)
+log_wL_data = log_wL_data0 + log_2p + na * log_rT
 log_wL_min, log_wL_max, N_L = init_w_axis(dxL, log_wL_data)
 
 
@@ -100,7 +107,7 @@ class initData(Structure):
         ("N_v", c_int),
         ("N_v_FT", c_int),
         ("N_x_FT", c_int),
-        # ("dxG", c_float),
+        ("dxG", c_float),
         ("dxL", c_float),
         ("N_lines", c_int),
         # ("N_coll", c_int),
@@ -112,16 +119,16 @@ class iterData(Structure):
     _fields_ = [
         # ("p", c_float),
         ("log_2p", c_float),
-        # ("hlog_T", c_float),
+        ("hlog_T", c_float),
         ("log_rT", c_float),
         ("c2T", c_float),
         ("N", c_float),
-        # ("x", c_float_arr_16),
+        ("x", c_float_arr_16),
         # ("l", c_float),
         # ("slit_FWHM", c_float),
-        # ("log_wG_min", c_float),
+        ("log_wG_min", c_float),
         ("log_wL_min", c_float),
-        # ("N_G", c_int),
+        ("N_G", c_int),
         ("N_L", c_int),
         # ("Q", c_float_arr_16),
     ]
@@ -131,7 +138,7 @@ def initialize():
     global app, data_in_d, data_FT_d, data_out_d, init_h
     print("Init... ", end="")
 
-    app = ComputeApplication(deviceID=0)
+    app = ComputeApplication(deviceID=1)
 
     # Buffers:
     app.init_h = initData()
@@ -152,10 +159,12 @@ def initialize():
     for arr in database_arrays:
         byte_offset += app.database_SSBO_d.setData(arr, byte_offset=byte_offset)
 
-    app.data_LDM_d = ArrayBuffer((N_L, N_v_FT), np.float32, binding=3, app=app)
+    app.data_LDM_d = ArrayBuffer((N_L, N_G, N_v_FT), np.float32, binding=3, app=app)
     # app.data_LDM_d.setData(LDM)
 
-    app.data_LDM_FT_d = ArrayBuffer((N_L, N_x_FT), np.complex64, binding=4, app=app)
+    app.data_LDM_FT_d = ArrayBuffer(
+        (N_L, N_G, N_x_FT), np.complex64, binding=4, app=app
+    )
     app.data_spectrum_FT_d = ArrayBuffer((N_x_FT,), np.complex64, binding=5, app=app)
     app.data_spectrum_d = ArrayBuffer((N_v_FT,), np.float32, binding=6, app=app)
 
@@ -197,17 +206,23 @@ app.init_h.N_lines = N_lines
 
 app.init_h.dv = dv
 app.init_h.v_min = v_min
+app.init_h.dxG = dxG
 app.init_h.dxL = dxL
 app.init_h.log_c2Mm = log_c2Mm_arr
 
 app.init_d.setData(app.init_h)
 
-app.iter_h.N_L = N_L
-app.iter_h.log_wL_min = log_wL_min
-app.iter_h.c2T = c2 / T0  # K
+
+app.iter_h.hlog_T = hlog_T
 app.iter_h.log_2p = np.log(2 * p0)
 app.iter_h.log_rT = np.log(296.0 / T0)
+app.iter_h.c2T = c2 / T0  # K
 app.iter_h.N = p0 * 1e5 / (1e6 * k * T0)  # cm-3
+app.iter_h.x = c_float_arr_16(x, 1 - x)
+app.iter_h.log_wG_min = log_wG_min
+app.iter_h.log_wL_min = log_wL_min
+app.iter_h.N_G = N_G
+app.iter_h.N_L = N_L
 
 app.iter_d.setData(app.iter_h)
 
@@ -215,14 +230,9 @@ app.iter_d.setData(app.iter_h)
 app.run()
 
 plt.axhline(0, c="k", lw=1, alpha=0.5)
-
 res = app.data_spectrum_d.getData()
 lines = plt.plot(v_arr[:N_v], res.T[:N_v], lw=0.5)
 plt.xlim(v_max, v_min)
-
-##I_ref = calc_spectrum(v_arr, v0_data, np.exp(log_w_data), E_data, T0)
-##plt.plot(v_arr[:N_v], I_ref[:N_v], "k--", lw=1)
-
 
 axw = plt.axes([0.25, 0.1, 0.65, 0.03])
 sw = Slider(axw, "T(K)", 300.0, 3000.0, valinit=T0)
@@ -230,13 +240,26 @@ sw = Slider(axw, "T(K)", 300.0, 3000.0, valinit=T0)
 
 def update(val):
     T = sw.val
-    app.iter_h.c2T = c2 / T
+
     app.iter_h.log_2p = np.log(2 * p0)
+    app.iter_h.hlog_T = 0.5 * np.log(T)
     app.iter_h.log_rT = np.log(296.0 / T)
+    app.iter_h.c2T = c2 / T
     app.iter_h.N = p0 * 1e5 / (1e6 * k * T)  # cm-3
 
+    log_wG_data = log_wG_data0 + app.iter_h.hlog_T
+    log_wL_data = log_wL_data0 + app.iter_h.log_2p + na * app.iter_h.log_rT
+
+    log_wG_min, log_wG_max, N_G = init_w_axis(dxG, log_wG_data)
+    log_wL_min, log_wL_max, N_L = init_w_axis(dxL, log_wL_data)
+
+    app.iter_h.log_wG_min = log_wG_min
+    app.iter_h.log_wL_min = log_wL_min
+    app.iter_h.N_G = N_G
+    app.iter_h.N_L = N_L
+
     app.iter_d.setData(app.iter_h)
-    app.data_LDM_d.setData(np.zeros((N_L, N_v_FT), dtype=np.float32))
+    app.data_LDM_d.setData(np.zeros((N_L, N_G, N_v_FT), dtype=np.float32))
     t0 = time.perf_counter()
     app.run()
     t1 = time.perf_counter()
