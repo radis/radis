eaffbd2fb7483cadc2222aecd635ce0f2ef18ab3
NicolasEM2C <nicolas.minesi@centralesupelec.fr>
2024-06-20 01:22:49 +0200

    delim_whitespace will be depreciated

--- radis/db/molparam.py
+++ radis/db/molparam.py
@@ -249,7 +249,7 @@ class MolParams(object):
         if file is None:
             file = getFile("molparam.txt")
 
-        df = pd.read_csv(file, comment="#", delim_whitespace=True)
+        df = pd.read_csv(file, comment="#", sep="\s+")
         df = df.set_index(["id", "iso"])
 
         df["isotope_name_exomol"] = _add_exomol_name(df)
