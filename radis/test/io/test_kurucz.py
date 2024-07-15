a6600fba43daf9b8ae7730feab918f904b3fa704
code29563 <89779096+code29563@users.noreply.github.com>
2024-07-15 07:56:12 +0100

    fix jlower jl

--- radis/test/io/test_kurucz.py
+++ radis/test/io/test_kurucz.py
@@ -9,10 +9,10 @@ from radis.test.utils import getTestFile
 @pytest.mark.fast
 def test_read_kurucz():
     df = read_kurucz(getTestFile('gf4000.all')) # from: http://kurucz.harvard.edu/atoms/4000/gf4000.all
-    a = df.loc[5, ['orig_wavelen', 'loggf', 'species', 'El', 'jlower', 'labellower', 'Eu', 'ju', 'labelupper', 'gamRad', 'gamSta', 'gamvdW', 'iso']]#.to_numpy()
+    a = df.loc[5, ['orig_wavelen', 'loggf', 'species', 'El', 'jl', 'labellower', 'Eu', 'ju', 'labelupper', 'gamRad', 'gamSta', 'gamvdW', 'iso']]#.to_numpy()
     b = np.array([504403.3610,-7.259,'40.00', 35456.250, 2.0, 'd3p w3P', 35476.070,3.0, '5s4F6s e5F', 8.41,-5.27,-7.53, 0], dtype='object')
     assert (a == b).all()
-    a = df.loc[7, ['orig_wavelen', 'loggf', 'species', 'El', 'jlower', 'labellower', 'Eu', 'ju', 'labelupper', 'gamRad', 'gamSta', 'gamvdW', 'iso']]#.to_numpy()
+    a = df.loc[7, ['orig_wavelen', 'loggf', 'species', 'El', 'jl', 'labellower', 'Eu', 'ju', 'labelupper', 'gamRad', 'gamSta', 'gamvdW', 'iso']]#.to_numpy()
     b = np.array([328425.5787 ,-6.296,'40.00',   21943.740,  2.0, 'd4 a5D',       21974.180,  1.0, 'd2sp z3S',    7.22, -5.99, -7.79, 0], dtype='object')
     assert (a == b).all()
 
