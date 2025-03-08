92561d58ea247bb6e8e426ec0b2a708e3d308a6a
Prtm2110 <prathamhole@gmail.com>
2025-03-08 23:02:55 +0530

    removed doc-string

--- radis/lbl/calc.py
+++ radis/lbl/calc.py
@@ -306,9 +306,7 @@ def calc_spectrum(
     return_factory: bool
         if ``True``, return the :py:class:`~radis.lbl.factory.SpectrumFactory` that
         computes the spectrum. Useful to access computational parameters, the line database,
-        or to start batch-computations from a first spectrum calculation.
-        By default, ``save_memory=True`` is set when using ``calc_spectrum``.
-        If you need to retain the full database without reloading, explicitly set ``save_memory=False``. Ex::
+        or to start batch-computations from a first spectrum calculation. Ex::
 
                 s, sf = calc_spectrum(..., return_factory=True, save_memory=False)
                 sf.df1  # see the lines calculated
