4df45f6d55c97d52d3e43765540a97c380cae8b3
NicolasEM2C <nicolas.minesi@centralesupelec.fr>
2025-03-25 09:23:30 +0100

    keep test vs Spectraplot the same

--- radis/test/validation/test_Kurucz_vs_SpectraPlot.py
+++ radis/test/validation/test_Kurucz_vs_SpectraPlot.py
@@ -30,7 +30,7 @@ def func1(**kwargs):
 def test_Kurucz_vs_NISTandSpectraplot(plot=True, verbose=True):
     def broad_arbitrary(**kwargs):
         """An arbitrary broadening formula in SpectraPlot (https://spectraplot.com/)"""
-        return 1 * (296 / kwargs["Tgas"]) ** 0.7, None
+        return 1 * (296 / kwargs["Tgas"]) ** 0.8, None
 
     #%% Experimental spectrum
     L = 1  # cm - Input in SpectraPlot software
