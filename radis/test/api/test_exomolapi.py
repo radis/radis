1d3ccfbd3b871440c65be663d05c5627515486b5
NicolasEM2C <nicolas.minesi@centralesupelec.fr>
2025-03-31 12:23:26 +0200

    remove cython comments and changed (again) engine to output

--- radis/test/api/test_exomolapi.py
+++ radis/test/api/test_exomolapi.py
@@ -1,25 +1,29 @@
+import pandas as pd
 import pytest
 
 from radis.api.exomolapi import check_code_level
 
 
+@pytest.mark.fast
 @pytest.mark.parametrize("bdat_list", [["a1"], ["a0", "a1"], ["a1", "a0"]])
 def test_check_bdat_a1(bdat_list):
-    bdat = {}
+    bdat = pd.DataFrame(bdat_list)
     bdat["code"] = bdat_list
-    assert check_code_level(bdat) == "a1"
+    assert check_code_level(bdat, output="pytables") == "a1"
 
 
+@pytest.mark.fast
 @pytest.mark.parametrize("bdat_list", [["a0"]])
 def test_check_bdat_a0(bdat_list):
-    bdat = {}
+    bdat = pd.DataFrame(bdat_list)
     bdat["code"] = bdat_list
-    assert check_code_level(bdat) == "a0"
+    assert check_code_level(bdat, output="pytables") == "a0"
 
 
+@pytest.mark.fast
 @pytest.mark.parametrize("bdat_list", [["a0", "a1", "a2"]])
 def test_check_bdat_no_code_level(bdat_list):
     """a2 is not a valid code level"""
-    bdat = {}
+    bdat = pd.DataFrame(bdat_list)
     bdat["code"] = bdat_list
-    assert check_code_level(bdat) == None
+    assert check_code_level(bdat, output="pytables") == None
