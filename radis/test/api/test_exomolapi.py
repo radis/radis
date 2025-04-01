25044ec899436bd8888cf65918e0e910e6564e57
NicolasEM2C <nicolas.minesi@centralesupelec.fr>
2025-04-01 10:52:33 +0200

    forgot to put back one line

--- radis/test/api/test_exomolapi.py
+++ radis/test/api/test_exomolapi.py
@@ -3,7 +3,7 @@ import pytest
 from radis.api.exomolapi import check_code_level
 
 
-@pytest.mark.NICOLAS
+@pytest.mark.fast
 @pytest.mark.parametrize("bdat_list", [["a1"], ["a0", "a1"], ["a1", "a0"]])
 def test_check_bdat_a1(bdat_list):
     bdat = {}
@@ -11,6 +11,7 @@ def test_check_bdat_a1(bdat_list):
     assert check_code_level(bdat) == "a1"
 
 
+@pytest.mark.fast
 @pytest.mark.parametrize("bdat_list", [["a0"]])
 def test_check_bdat_a0(bdat_list):
     bdat = {}
@@ -18,6 +19,8 @@ def test_check_bdat_a0(bdat_list):
     assert check_code_level(bdat) == "a0"
 
 
+@pytest.mark.fast
+@pytest.mark.parametrize("bdat_list", [["a0", "a1", "a2"]])
 def test_check_bdat_no_code_level(bdat_list):
     """a2 is not a valid code level"""
     bdat = {}
