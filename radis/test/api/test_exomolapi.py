3ad55d8cb67fc957b88e4e6e113faf471da97e7c
NicolasEM2C <nicolas.minesi@centralesupelec.fr>
2025-04-01 09:49:26 +0200

    pytest incompatibility with empty dataframe?

--- radis/test/api/test_exomolapi.py
+++ radis/test/api/test_exomolapi.py
@@ -5,23 +5,20 @@ from radis.api.exomolapi import check_code_level
 
 
 @pytest.mark.fast
-def test_check_bdat_a1():
+def test_check_code_level_a1():
     for code in [["a1"], ["a0", "a1"], ["a1", "a0"]]:
-        bdat = pd.DataFrame()
-        bdat["code"] = code
+        bdat = pd.DataFrame({"code": code})
         assert check_code_level(bdat) == "a1"
 
 
 @pytest.mark.fast
-def test_check_bdat_a0():
-    bdat = pd.DataFrame()
-    bdat["code"] = ["a0"]
+def test_check_code_level_a0():
+    bdat = pd.DataFrame({"code": ["a0"]})
     assert check_code_level(bdat) == "a0"
 
 
 @pytest.mark.fast
-def test_check_bdat_no_code_level():
+def test_check_no_code_level():
     """a2 is not a valid code level"""
-    bdat = pd.DataFrame()
-    bdat["code"] = ["a0", "a1", "a2"]
+    bdat = pd.DataFrame({"code": ["a0", "a1", "a2"]})
     assert check_code_level(bdat) == None
