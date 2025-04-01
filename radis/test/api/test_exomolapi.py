911bd2a6bdbf4703bb0be27f8cae363c0e4d75a5
NicolasEM2C <nicolas.minesi@centralesupelec.fr>
2025-04-01 10:01:24 +0200

    revert to original test with new argument in assert

--- radis/test/api/test_exomolapi.py
+++ radis/test/api/test_exomolapi.py
@@ -5,20 +5,24 @@ from radis.api.exomolapi import check_code_level
 
 
 @pytest.mark.fast
-def test_check_code_level_a1():
-    for code in [["a1"], ["a0", "a1"], ["a1", "a0"]]:
-        bdat = pd.DataFrame({"code": code})
-        assert check_code_level(bdat) == "a1"
+@pytest.mark.parametrize("bdat_list", [["a1"], ["a0", "a1"], ["a1", "a0"]])
+def test_check_bdat_a1(bdat_list):
+    bdat = {}
+    bdat["code"] = bdat_list
+    assert check_code_level(pd.DataFrame(bdat)) == "a1"
 
 
 @pytest.mark.fast
-def test_check_code_level_a0():
-    bdat = pd.DataFrame({"code": ["a0"]})
-    assert check_code_level(bdat) == "a0"
+@pytest.mark.parametrize("bdat_list", [["a0"]])
+def test_check_bdat_a0(bdat_list):
+    bdat = {}
+    bdat["code"] = bdat_list
+    assert check_code_level(pd.DataFrame(bdat)) == "a0"
 
 
 @pytest.mark.fast
-def test_check_no_code_level():
+def test_check_bdat_no_code_level(bdat_list):
     """a2 is not a valid code level"""
-    bdat = pd.DataFrame({"code": ["a0", "a1", "a2"]})
-    assert check_code_level(bdat) == None
+    bdat = {}
+    bdat["code"] = bdat_list
+    assert check_code_level(pd.DataFrame(bdat)) == None
