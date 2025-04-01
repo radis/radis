10ba0361643e9a20f030b185047504e4a17461e2
NicolasEM2C <nicolas.minesi@centralesupelec.fr>
2025-04-01 10:11:36 +0200

    revert to original test

--- radis/test/api/test_exomolapi.py
+++ radis/test/api/test_exomolapi.py
@@ -1,28 +1,24 @@
-import pandas as pd
 import pytest
 
 from radis.api.exomolapi import check_code_level
 
 
-@pytest.mark.fast
 @pytest.mark.parametrize("bdat_list", [["a1"], ["a0", "a1"], ["a1", "a0"]])
 def test_check_bdat_a1(bdat_list):
     bdat = {}
     bdat["code"] = bdat_list
-    assert check_code_level(pd.DataFrame(bdat)) == "a1"
+    assert check_code_level(bdat) == "a1"
 
 
-@pytest.mark.fast
 @pytest.mark.parametrize("bdat_list", [["a0"]])
 def test_check_bdat_a0(bdat_list):
     bdat = {}
     bdat["code"] = bdat_list
-    assert check_code_level(pd.DataFrame(bdat)) == "a0"
+    assert check_code_level(bdat) == "a0"
 
 
-@pytest.mark.fast
 def test_check_bdat_no_code_level(bdat_list):
     """a2 is not a valid code level"""
     bdat = {}
     bdat["code"] = bdat_list
-    assert check_code_level(pd.DataFrame(bdat)) == None
+    assert check_code_level(bdat) == None
