3ffc07f05fc3c6af5119d0217cedfd681575c3dc
Darshan Patil <darshanpatil15105@gmail.com>
2025-07-19 12:26:41 +0530

    add electronic spectra example

---
+++ examples/1_Spectra_handling/plot_exomol_electronic_noneq_spectrum.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Example: Calculate and plot the non-equilibrium electronic spectrum of an ExoMol molecule (e.g., OH) using RADIS.
+
+This script demonstrates:
+- Calculating non-equilibrium spectra with Telec != Tvib = Trot
+- Manually adjusting electronic band intensities (band scaling)
+- Accessing and plotting populations for each electronic state
+
+You can adapt this script for any ExoMol molecule by changing the database and .states file
+"""
+
+from radis import SpectrumFactory
+
+sf = SpectrumFactory(
+    wavelength_min=300,
+    wavelength_max=340,
+    molecule="OH",
+    isotope="1",
+    pressure=1,
+    path_length=1,
+    mole_fraction=1e-3,
+    wstep=0.001,
+    verbose=True,
+    self_absorption=True
+)
+
+sf.fetch_databank("exomol", "MoLLIST-OH")
+sf.misc.export_rovib_fraction = True
+
+s = sf.non_eq_spectrum(
+    Tvib=Tvib,
+    Trot=Trot,
+    Telec=Telec,
+    mole_fraction=mole_fraction,
+    path_length=path_length,
+    pressure=pressure
+    # band_scaling={"A": 2.0, "X": 1.0}
+)
+s.apply_slit(0.1)
+s.plot('radiance')
