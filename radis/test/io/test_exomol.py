6eb38aee5ca96820487a8e7a1ce2ff9167bfcb56
Prtm2110 <prathamhole@gmail.com>
2025-10-06 19:47:35 +0530

    dublicate dec removed

--- radis/test/io/test_exomol.py
+++ radis/test/io/test_exomol.py
@@ -83,7 +83,6 @@ def test_calc_exomol_spectrum(verbose=True, plot=True, *args, **kwargs):
         s.plot()
 
 
-@pytest.mark.needs_HITRAN_credentials
 @pytest.mark.needs_connection
 @pytest.mark.needs_HITRAN_credentials
 def test_calc_exomol_vs_hitemp(verbose=True, plot=True, *args, **kwargs):
