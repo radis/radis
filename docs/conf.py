91e197aa70fcdcf02af516f89cebff8b62e42f44
Anmol Janamatti <84708393+anmol-janamatti@users.noreply.github.com>
2025-12-07 23:40:48 +0530

    docs: add Sphinx documentation configuration and setup.

--- docs/conf.py
+++ docs/conf.py
@@ -52,6 +52,7 @@ sphinx_gallery.gen_rst.EXAMPLE_HEADER = """
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../radis")) # Attempt to fix import errors for submodules
 
 
 # %% ------------------------------------
