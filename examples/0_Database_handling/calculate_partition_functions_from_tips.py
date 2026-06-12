aac9c4ec0f9fc435960ad9849e6da84ef8072c44
prathamhole14 <prathamhole@gmail.com>
2026-06-12 15:20:29 +0530

    add missing labels and title for sphinx warnings

--- examples/0_Database_handling/calculate_partition_functions_from_tips.py
+++ examples/0_Database_handling/calculate_partition_functions_from_tips.py
@@ -4,6 +4,8 @@
 Partition Functions from TIPS
 =============================
 
+.. _label_examples_partition_functions:
+
 By default and for equilibrium calculations, RADIS calculates Partition Functions
 using the TIPS program ([TIPS-2020]_) through [HAPI]_. These partition functions can be retrieved
 with the :py:class:`~radis.levels.partfunc.PartFuncTIPS` class. See also :py:class:`~radis.levels.partfunc.PartFunc_Dunham`.
