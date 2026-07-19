9333f5dbcb56017a7e06129d4e9c1f1c0d4f5d0e
Dirk van den Bekerom <dcmvdbekerom@gmail.com>
2026-07-19 10:05:51 -0400

    linux shell script

--- .github/proxy/start_proxy_linux.sh
+++ .github/proxy/start_proxy_linux.sh
@@ -4,10 +4,10 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/CN=www.hitran.org" -
 
 export NGINX_CACHE_DIR=/home/runner/work/radis/radis/nginx_cache
 envsubst '$NGINX_CACHE_DIR' < .github/proxy/nginx.conf > .github/proxy/nginx.conf.gen
-sudo nginx -c ${{ github.workspace }}/.github/proxy/nginx.conf.gen -g "user $(whoami);"&
+sudo nginx -c $GITHUB_WORKSPACE/.github/proxy/nginx.conf.gen -g "user $(whoami);"&
 
 echo "127.0.0.1 www.hitran.org  hitran.org" | sudo tee -a /etc/hosts
 
-sudo cp "${{ github.workspace }}/cert.pem" /usr/local/share/ca-certificates/nginx-proxy.crt
+sudo cp "$GITHUB_WORKSPACE/cert.pem" /usr/local/share/ca-certificates/nginx-proxy.crt
 sudo update-ca-certificates
 echo "REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt" >> $GITHUB_ENV
